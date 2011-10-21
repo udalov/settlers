5114d52c2ca726737fda6d57e960b5223b75b81c
Alexander Udalov <udalov.alexander@gmail.com>
2011-10-21 21:45:24 +0400

    Resource.all(), Util.numberOfOccurrences()

--- src/settlers/util/Util.java
+++ src/settlers/util/Util.java
@@ -4,9 +4,9 @@ public class Util {
     
     private Util() { }
 
-    public static int numberOfOccurances(char needle, String haystack) {
+    public static int numberOfOccurrences(char needle, String haystack) {
         int ans = 0;
-        for (int i = 0; i < haystack.length; i++)
+        for (int i = 0; i < haystack.length(); i++)
             if (haystack.charAt(i) == needle)
                 ans++;
         return ans;
