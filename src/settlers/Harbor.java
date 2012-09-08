3aa79bfa13b5ed189ff8bbe294bc94d74b23fba9
Alexander Udalov <udalov.alexander@gmail.com>
2012-09-08 14:25:18 +0400

    add toString(), hashCode(), equals() to Harbor

--- src/settlers/Harbor.java
+++ src/settlers/Harbor.java
@@ -9,5 +9,17 @@ public class Harbor {
     }
 
     public Resource resource() { return resource; }
+
+    public String toString() {
+        return resource == null ? "3:1" : resource.toString();
+    }
+
+    public int hashCode() {
+        return resource == null ? 0 : resource.ordinal() + 1;
+    }
+
+    public boolean equals(Object o) {
+        return o instanceof Harbor && ((Harbor)o).resource == resource;
+    }
 }
 
